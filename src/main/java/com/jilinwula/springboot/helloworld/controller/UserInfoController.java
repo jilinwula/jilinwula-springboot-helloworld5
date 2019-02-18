897ee780555775ac4f4fafa6a58e112fe2fbf4e9
吉林乌拉 <admin@jilinwula.com>
2019-02-18 14:39:28 +0800

    SpringBoot参数验证

--- src/main/java/com/jilinwula/springboot/helloworld/controller/UserInfoController.java
+++ src/main/java/com/jilinwula/springboot/helloworld/controller/UserInfoController.java
@@ -3,10 +3,13 @@ package com.jilinwula.springboot.helloworld.controller;
 import com.jilinwula.springboot.helloworld.Repository.UserInfoRepository;
 import com.jilinwula.springboot.helloworld.entity.UserInfoEntity;
 import com.jilinwula.springboot.helloworld.query.UserInfoQuery;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
--- src/main/java/com/jilinwula/springboot/helloworld/query/UserInfoQuery.java
+++ src/main/java/com/jilinwula/springboot/helloworld/query/UserInfoQuery.java
@@ -6,10 +6,15 @@ import org.springframework.stereotype.Component;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Component
 @Data
 public class UserInfoQuery{
+
+    @NotNull(message = "用户编号不能为空")
+    @Pattern(regexp = "^[1-10]$",message = "用户编号范围不正确")
+    private String id;
 
     @NotNull(message = "账号不能为空")
     private String username;
